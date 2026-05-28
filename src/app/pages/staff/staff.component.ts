2229f7e99ea17e656ef6668712d6a112bc1710d4
lexxiij <aljdrpamj@yahoo.com>
2026-05-27 19:57:06 -0500

    feat: move staff photo to Pam Jones card on staff page

--- src/app/pages/staff/staff.component.ts
+++ src/app/pages/staff/staff.component.ts
@@ -13,6 +13,7 @@ export class StaffComponent {
       title: 'Executive Director & Founder',
       bio: 'Mrs. Pamela Jones is the founder and Executive Director of MTN, Inc. She is a Charleston, MO native. Mrs. Jones graduated with her B.S. degree in Business Administration from Hannibal LeGrange University. Mrs. Pam is an entrepreneur and has worked with non-profit organizations for twenty-five years helping to improve the lives of others.',
       icon: 'fas fa-user-tie',
+      photo: 'assets/photos/meetthestaff.jpg',
       contact: '573-233-2980'
     }
     // Add more staff members here as the organization grows!
